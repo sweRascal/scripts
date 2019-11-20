1abf0575eac800d75108d1494508f608722af642
André Arvidsson <sweRascal@users.noreply.github.com>
2019-11-20 11:43:04 +0100

    Add files via upload

---
+++ fix_google_chrome.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Small script that changes the shortcut to Google Chrome on macOS disabeling the async-dns so that adblockers like PiHole work properly
+
+mv /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome-bin
+touch /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome 
+echo '#!/bin/sh' >> /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+echo 'exec /Applications/Google Chrome.app/Contents/MacOS/Google Chrome-bin --args --disable-async-dns' >> /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+chmod +x /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
