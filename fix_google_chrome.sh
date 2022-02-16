fea0fd2a2fbbc7fce651f42e174979c9274adb89
borjarobert <54457920+borjarobert@users.noreply.github.com>
2022-02-16 23:45:40 +0100

    a couple of spaces needed escaping
    In the 8th line Google\ Chrome needs escaping the space (twice)

--- fix_google_chrome.sh
+++ fix_google_chrome.sh
@@ -5,5 +5,5 @@
 mv /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome-bin
 touch /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome 
 echo '#!/bin/sh' >> /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
-echo 'exec /Applications/Google Chrome.app/Contents/MacOS/Google Chrome-bin --args --disable-async-dns' >> /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+echo 'exec /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome-bin --args --disable-async-dns' >> /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
 chmod +x /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
